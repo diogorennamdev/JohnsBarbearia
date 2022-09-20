1994998649634f0ea7afe32efeaae534451c5f36
ribeial <victoralves169@outlook.com>
2022-09-20 16:38:50 -0300

    identação no código digitado

--- JohnsBarbearia/src/DAO/ConexaoDAO.java
+++ JohnsBarbearia/src/DAO/ConexaoDAO.java
@@ -3,21 +3,19 @@ package DAO;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-        
 
 public class ConexaoDAO {
-    
+
     Connection conn;
-    
+
     public Connection conectaBD() {
         try {
             String url = "jdbc:mysql://localhost:3306/JohnsBarbearia?user=root&password=";
-                    conn = DriverManager.getConnection(url);
-        }
-        catch (SQLException ex){
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException ex) {
             System.out.println("Deu ruim na conexão" + ex);
         }
         return conn;
     }
-    
+
 }
