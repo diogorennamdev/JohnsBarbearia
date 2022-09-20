9972d2bab108ebeb97b4f1ef10ffb90aa2644ef8
ribeial <victoralves169@outlook.com>
2022-09-20 18:59:35 -0300

    identação do código

--- JohnsBarbearia/src/DTO/UsuarioDTO.java
+++ JohnsBarbearia/src/DTO/UsuarioDTO.java
@@ -2,13 +2,9 @@ package DTO;
 
 public class UsuarioDTO {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
-    //tetstando//
-        
+
     }
 
 }
