1c29e6c08de97e133811839b24fca2e7cd1c8cc7
Diogo Rennam <diogorenan006@gmail.com>
2022-09-20 22:26:43 -0300

    Atualizando AgendamentoDTO e UsuarioDto

--- JohnsBarbearia/src/DTO/AgendamentoDTO.java
+++ JohnsBarbearia/src/DTO/AgendamentoDTO.java
@@ -1,14 +1,51 @@
 package DTO;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import java.util.Date;
 
-/**
- *
- * @author Diogo
- */
 public class AgendamentoDTO {
+    private String nome_cliente;
+    private Date data;
+    private String servico;
+    private float valor_servico;
+
+    public AgendamentoDTO(String nome_cliente, Date data, String servico, float valor_servico) {
+        this.nome_cliente = nome_cliente;
+        this.data = data;
+        this.servico = servico;
+        this.valor_servico = valor_servico;
+    }
+
+    public String getNome_cliente() {
+        return nome_cliente;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public String getServico() {
+        return servico;
+    }
+
+    public float getValor_servico() {
+        return valor_servico;
+    }
+
+    public void setNome_cliente(String nome_cliente) {
+        this.nome_cliente = nome_cliente;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public void setServico(String servico) {
+        this.servico = servico;
+    }
+
+    public void setValor_servico(float valor_servico) {
+        this.valor_servico = valor_servico;
+    }
+    
     
 }
--- JohnsBarbearia/src/DTO/UsuarioDTO.java
+++ JohnsBarbearia/src/DTO/UsuarioDTO.java
@@ -2,9 +2,54 @@ package DTO;
 
 public class UsuarioDTO {
 
-    public static void main(String[] args) {
-        // TODO code application logic here
+   private String nome;
+   private String email;
+   private String tipo;
+   private int id;
+   private String senha;
 
+    public UsuarioDTO(String nome, String email, String tipo, int id, String senha) {
+        this.nome = nome;
+        this.email = email;
+        this.tipo = tipo;
+        this.id = id;
+        this.senha = senha;
     }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getsenha(){
+        return senha;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+   public void setSenha(String senha){
+       this.senha = senha;
+   }
 }
