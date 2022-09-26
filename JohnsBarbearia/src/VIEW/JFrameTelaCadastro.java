e7519ca15d5800bb7082de79c2ca2f09d8ec76fc
ribeial <victoralves169@outlook.com>
2022-09-26 15:57:10 -0300

    estilização da telas

--- JohnsBarbearia/src/VIEW/JFrameTelaCadastro.java
+++ JohnsBarbearia/src/VIEW/JFrameTelaCadastro.java
@@ -1,7 +1,4 @@
-
 package VIEW;
-
-import java.util.Set;
 
 public class JFrameTelaCadastro extends javax.swing.JFrame {
 
@@ -240,7 +237,7 @@ public class JFrameTelaCadastro extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-         Set the Nimbus look and feel 
+        /* Set the Nimbus look and feel*/
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
