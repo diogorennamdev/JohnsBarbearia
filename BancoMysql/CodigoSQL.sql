f06fcb92468c0450f6c70fb3c57814a61d38e573
ribeial <victoralves169@outlook.com>
2022-10-15 01:47:28 -0300

    update no BancoMysql

--- BancoMysql/CodigoSQL.sql
+++ BancoMysql/CodigoSQL.sql
@@ -1,19 +1,15 @@
 create database if not exists JohnsBarbearia;
 use JohnsBarbearia;
 create table if not exists usuario(
-  id_usuario int auto_increment primary key,
-  email_usuario varchar (30) ,
-  senha_usuario varchar (90),
+  CPF_usuario varchar(11) primary key NOT NULL,
   nome_usuario varchar (30),
+  email_usuario varchar (30) NOT NULL,
+  senha_usuario varchar (90),
   tipo_usuario varchar (15)
   );
 create table if not exists Agendamento(
-	nome_cliente varchar (20) NOT NULL, 
-  	data_agendamento DATE,
-  	servico varchar (30),
-    valor_servico varchar (20) 
-);
-
-
-
-
+  nome_cliente varchar (20) primary key NOT NULL,
+  data_agendamento DATE,
+  servico varchar (30),
+  valor_servico varchar (20) 
+  );
