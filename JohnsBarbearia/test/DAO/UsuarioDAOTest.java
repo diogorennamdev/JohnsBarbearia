9aa6c664f71a8f7cc4a49deb1d00cc704c27fe20
Diogo Rennam <diogorenan006@gmail.com>
2022-10-25 12:36:12 -0300

    Adicionando testes para inserir e validar usuario

--- JohnsBarbearia/test/DAO/UsuarioDAOTest.java
+++ JohnsBarbearia/test/DAO/UsuarioDAOTest.java
@@ -1,27 +1,64 @@
-
 package DAO;
 
 import DTO.UsuarioDTO;
+import DAO.UsuarioDAO;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 
 public class UsuarioDAOTest {
-    
+
+    private static final String SENHA = "1234";
+    private static final String EMAIL = "diogo@gmail.com";
+    private static final String NAME = "Diogo";
+    private static final String CPF = "0124789";
+
+    private UsuarioDTO usuario = new UsuarioDTO();
+    private UsuarioDAO usuariodao = new UsuarioDAO();
+
     public UsuarioDAOTest() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+
+    }
+
+    @Before
+    public void setUp() {
+        startUser();
+    }
+
+    @After
+    public void tearDown() {
     }
 
     @Test
     public void testAutenticacaoUsuario() {
+        usuariodao.autenticacaoUsuario(usuario);
+        assertNotNull(usuario);
+        assertEquals(EMAIL, usuario.getEmail_usuario());
+        assertEquals(SENHA,usuario.getSenha_usuario());
     }
 
     @Test
-    public void testCadastrarUsuario() {
-        UsuarioDTO usuario = new UsuarioDTO("45767987",
-                "Diogo", "diogo@gmail.com", "1234");
-       
-        UsuarioDAO usuariodao = new UsuarioDAO();
+    public void TesteCadastrarUsuario() {
         usuariodao.CadastrarUsuario(usuario);
+   
+        assertEquals(CPF, usuario.getCPF_usuario());
+        assertEquals(NAME, usuario.getNome_usuario());
+        assertEquals(EMAIL, usuario.getEmail_usuario());
+        assertEquals(SENHA, usuario.getSenha_usuario());
+
     }
-    
+
+    private void startUser() {
+        usuario = new UsuarioDTO(CPF, NAME,
+                EMAIL, SENHA);
+
+    }
+
 }
