f863a2f101610ed8d687f4bfb34a773cf1fd915f
ribeial <victoralves169@outlook.com>
2022-11-07 22:13:57 -0300

    Retirei do Banco de dados email_usuario e tipo_usuario

--- BancoMysql/CodigoSQL.sql
+++ BancoMysql/CodigoSQL.sql
@@ -3,9 +3,7 @@ use JohnsBarbearia;
 create table if not exists usuario(
   CPF_usuario varchar(11) primary key NOT NULL,
   nome_usuario varchar (30),
-  email_usuario varchar (30) NOT NULL,
   senha_usuario varchar (90),
-  tipo_usuario varchar (15)
   );
 create table if not exists Agendamento(
   nome_cliente varchar (20) primary key NOT NULL,
