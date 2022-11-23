607e3fc08e7066cf9f95f927164c400ab65c2afe
ribeial <victoralves169@outlook.com>
2022-11-23 18:49:26 -0300

    adcionei ID_agendamento e defini ele com chave primaria

--- BancoMysql/CodigoSQL.sql
+++ BancoMysql/CodigoSQL.sql
@@ -1,15 +1,18 @@
 create database if not exists JohnsBarbearia;
 use JohnsBarbearia;
 create table if not exists usuario(
-  CPF_usuario varchar(11) primary key NOT NULL,
-  nome_usuario varchar (30),
-  senha_usuario varchar (90)
+	CPF_usuario varchar(11) NOT NULL,
+	nome_usuario varchar (30),
+	senha_usuario varchar (90),
+	PRIMARY KEY (CPF_usuario)
   );
 create table if not exists Agendamento(
-  nome_cliente varchar (30) primary key NOT NULL,
-  servico varchar (90),
-  valor_servico varchar (20), 
-  data_agendamento varchar (10),
-  hora_agendamento varchar (5),
-  observacao_agendamento varchar(50) 
+	ID_agendamento int NOT NULL AUTO_INCREMENT,
+	nome_cliente varchar (30),
+	servico varchar (90),
+	valor_servico varchar (20),
+	data_agendamento varchar (10),
+	hora_agendamento varchar (5),
+	observacao_agendamento varchar(50),
+	PRIMARY KEY (ID_agendamento)
   );
