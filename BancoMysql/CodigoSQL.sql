aceb6d7b09173289dfa6a49a629731af09cae157
ribeial <victoralves169@outlook.com>
2022-11-23 22:10:40 -0300

    adicionei uma chave estrangeira em agendamento

--- BancoMysql/CodigoSQL.sql
+++ BancoMysql/CodigoSQL.sql
@@ -1,18 +1,18 @@
 create database if not exists JohnsBarbearia;
 use JohnsBarbearia;
 create table if not exists usuario(
-	CPF_usuario varchar(11) NOT NULL,
+	CPF_usuario varchar(11) NOT NULL PRIMARY KEY,
 	nome_usuario varchar (30),
 	senha_usuario varchar (90),
-	PRIMARY KEY (CPF_usuario)
   );
 create table if not exists Agendamento(
-	ID_agendamento int NOT NULL AUTO_INCREMENT,
+	ID_agendamento int NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	nome_cliente varchar (30),
 	servico varchar (90),
 	valor_servico varchar (20),
 	data_agendamento varchar (10),
 	hora_agendamento varchar (5),
 	observacao_agendamento varchar(50),
-	PRIMARY KEY (ID_agendamento)
+	CPF_uduario varchar (11),
+	FOREING KEY (CPF_uduario) REFERENCES usuario(CPF_uduario)
   );
