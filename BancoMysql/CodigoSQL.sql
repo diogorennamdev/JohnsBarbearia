98b52332666dc0fbad86a294015b98ae3b2bdae1
ribeial <victoralves169@outlook.com>
2022-11-24 21:50:40 -0300

    retirei CPF_usuario da tabela Agendamento e referenciei como chave estrangeira ID_agendamento

--- BancoMysql/CodigoSQL.sql
+++ BancoMysql/CodigoSQL.sql
@@ -3,7 +3,7 @@ use JohnsBarbearia;
 create table if not exists usuario(
 	CPF_usuario varchar(11) NOT NULL PRIMARY KEY,
 	nome_usuario varchar (30),
-	senha_usuario varchar (90),
+	senha_usuario varchar (90)
   );
 create table if not exists Agendamento(
 	ID_agendamento int NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -13,6 +13,5 @@ create table if not exists Agendamento(
 	data_agendamento varchar (10),
 	hora_agendamento varchar (5),
 	observacao_agendamento varchar(50),
-	CPF_usuario varchar (11),
-	FOREIGN KEY (CPF_usuario) REFERENCES usuario(CPF_usuario)
+	FOREIGN KEY (ID_agendamento) REFERENCES usuario(CPF_usuario)
   );
